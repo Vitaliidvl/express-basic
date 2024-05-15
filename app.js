84e0a31317ecaba4cc62e55aab2981b5a888c3b6
Vitalii <kicker198686@gmail.com>
2024-05-15 09:30:12 +0300

    404 added

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/404');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: '404' });
-});
+app.use(errorController.get404);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
